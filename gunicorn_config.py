7430c640a9767446ae94a47347e59cc054d874a1
Rob McQueen <rob@mopub.com>
2013-02-04 20:21:54 -0800

    adding gunicorn config and gunicorn to requirements

---
+++ gunicorn_config.py
@@ -0,0 +1,19 @@
+# see http://gunicorn.org/configure.html                                                                                                                                          
+
+import os
+#import multiprocessing                                                                                                                                                           
+
+__pwd = os.path.abspath(__file__)
+
+
+# Serving                                                                                                                                                                         
+
+# This needs to be high (>10) in order to support multiple stats calls                                                                                                            
+# from the browser                                                                                                                                                                
+workers = 5 #multiprocessing.cpu_count() * 2 + 1                                                                                                                                 
+timeout = 120
+bind = "127.0.0.1:8000"
+
+# Logging                                                                                                                                                                         
+
+loglevel = 'debug'
