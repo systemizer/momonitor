e1a39713dd92074cdf5694f0d308c54d588011a8
Rob McQueen <rob@mopub.com>
2013-03-31 20:07:22 -0700

    fixing form modal titles

--- main/forms/utils.py
+++ main/forms/utils.py
@@ -2,7 +2,7 @@ from momonitor.main.forms.service_check import ServiceCheckForm
 
 def generate_check_modelform_cls(model_cls,template_path=None):
     class CheckForm(ServiceCheckForm):
-        title="Create/Edit %s " % str(model_cls)
+        title="Create/Edit %s " % model_cls.__name__
         template = template_path or "main/modal_forms/servicecheck.html"
         class Meta:
             model = model_cls
